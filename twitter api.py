1d94c0c7e42df807e0301661e428fe07fa1b9e17
Bikram300 <32117369+Bikram300@users.noreply.github.com>
2018-07-13 01:51:29 -0400

    Update twitter api.py

--- twitter api.py
+++ twitter api.py
@@ -9,10 +9,10 @@ print("6.Analyre top usage")
 print("7.Tweet a message")
 print("8.exit")
 choice=int(input("Enter your choice: "))
-consumer_key='nUryARrGKjKe9WAsiNn4XzkFI'
-cosumer_secret='78ME9d7ed8wvAYzOz0q2F7y4sCz88JBUskdBwhbPoFo5hoSi93'
-access_token='1011137809758019584-CkwfiGz6yPc64fRLLvcUCcUJN4oC9H'
-access_token_secret='rbG4oU4P5l6jujaYoeXVVJbrQKqb2lC422IPW21ebegka'
+consumer_key='######'
+cosumer_secret='########'
+access_token='#########'
+access_token_secret='##########'
 auth=tweepy.OAuthHandler(consumer_key,cosumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 api=tweepy.API(auth)
